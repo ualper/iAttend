9ca9c82969de5d0a67b76b789076a7887e179932
UMUT AY ALPER <55507463+ualper@users.noreply.github.com>
2024-07-07 19:36:41 +0300

    Delete iattend directory

--- iattend/android/app/src/main/kotlin/com/example/face_auth/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ualper.iattend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- iattend/lib/common/utils/extensions/size_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:iattend/common/utils/screen_size_util.dart';
-
-extension SizeExtension on num {
-  /// Screen Width Extension Value
-  double get sw => ScreenSizeUtil.screenWidth * this;
-
-  /// Screen Height Extension Value
-  double get sh => ScreenSizeUtil.screenHeight * this;
-}
